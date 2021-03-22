63767274614ed6e79ca27280022493158a641f9b
masesm5@yahoo.com <maseskrikorian@gmail.com>
2021-03-21 19:10:50 -0700

    v0.1 build

---
+++ build/precache-manifest.b9c64ffacfb285608f9af6de61625c7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a86b0dd8099331838c47611ea1ccfcab",
+    "url": "./index.html"
+  },
+  {
+    "revision": "035dd8accb7c3bc73aaa",
+    "url": "./static/css/main.b40fca9d.chunk.css"
+  },
+  {
+    "revision": "280c216cc7c500bfd3d2",
+    "url": "./static/js/2.df35a9fc.chunk.js"
+  },
+  {
+    "revision": "47a00163856d2c9df4d563d6a5a069d2",
+    "url": "./static/js/2.df35a9fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "035dd8accb7c3bc73aaa",
+    "url": "./static/js/main.5afd29b5.chunk.js"
+  },
+  {
+    "revision": "3039c9b6180ebe2ee42c",
+    "url": "./static/js/runtime-main.c9354e16.js"
+  }
+]);
